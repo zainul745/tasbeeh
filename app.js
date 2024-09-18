e6bf53a5fdecd95839b2fd4c005df885a09bb234
zainul745 <abdinzainul745@gmail.com>
2024-09-19 00:06:21 +0900

    kbw

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDZcuQmv6W-n_EiLw_5i0qU6nIV1_M_VSU",
+  authDomain: "zainulabideen-95d8b.firebaseapp.com",
+  projectId: "zainulabideen-95d8b",
+  storageBucket: "zainulabideen-95d8b.appspot.com",
+  messagingSenderId: "650585851882",
+  appId: "1:650585851882:web:12c40a2398e358677c9493"
+};
 firebase.initializeApp(firebaseConfig);
 var auth = firebase.auth();
 var db = firebase.firestore();
@@ -9,14 +16,14 @@ var subhanallah = 0;
 var alhamdulillah = 0;
 var kalmaShareef = 0;
 var duroodEPak = 0;
-var activeTasbeeh = "";
+var acti_Tasbeeh = "";
 
 function homeclick(){
     var all_dispaly = document.getElementById("all_dispaly");
   var tasbeeh_section = document.getElementById("tasbeeh_section");
   all_dispaly.classList.remove("d-none");
   tasbeeh_section.classList.add("d-none");
-  console.log("Daboard");
+  console.log("yes");
 }
 
 function showtasbeeh(tasbeeh){
@@ -32,49 +39,48 @@ function showtasbeeh(tasbeeh){
   tasbeeh_section.classList.remove("d-none");
   var hading = document.getElementById("tasbeeh_h")
   var count = document.getElementById("tasbeeh_c")
-  activeTasbeeh = tasbeeh;
+  acti_Tasbeeh = tasbeeh;
   hading.innerHTML = tasbeeh
   count.innerHTML = allTasbeeh[tasbeeh]
-  console.log("tasbeeh")
+  console.log("tasbe")
 }
 
-function changenumber(type) {
+function changenumber(number) {
     var count = document.getElementById("tasbeeh_c");
-    if (type === "plus") {
-      if (activeTasbeeh === "subhanallah") {
+    if (number === "+") {
+      if (acti_Tasbeeh === "subhanallah") {
         subhanallah++;
         count.innerHTML = subhanallah;
       }
-      if (activeTasbeeh === "alhamdulillah") {
+      if (acti_Tasbeeh === "alhamdulillah") {
         alhamdulillah++;
         count.innerHTML = alhamdulillah;
       }
-      if (activeTasbeeh === "kalmaShareef") {
+      if (acti_Tasbeeh === "kalmaShareef") {
         kalmaShareef++;
         count.innerHTML = kalmaShareef;
       }
-      if (activeTasbeeh === "duroodEPak") {
+      if (acti_Tasbeeh === "duroodEPak") {
         duroodEPak++;
         count.innerHTML = kalmaShareef;
       }
-    } else {
-      if (Number(count.innerHTML) > 0) {
-        if (activeTasbeeh === "subhanallah") {
-          subhanallah--;
-          count.innerHTML = subhanallah;
-        }
-        if (activeTasbeeh === "alhamdulillah") {
-          alhamdulillah--;
-          count.innerHTML = alhamdulillah;
-        }
-        if (activeTasbeeh === "kalmaShareef") {
-          kalmaShareef--;
-          count.innerHTML = kalmaShareef;
-        }
-        if (activeTasbeeh === "duroodEPak") {
-          duroodEPak--;
-          count.innerHTML = kalmaShareef;
-        }
+    } 
+    if (number === "-") {
+      if (acti_Tasbeeh === "subhanallah") {
+        subhanallah--
+        count.innerHTML = subhanallah;
       }
-    }
+      if (acti_Tasbeeh === "alhamdulillah") {
+        alhamdulillah--
+        count.innerHTML = alhamdulillah;
+      }
+      if (acti_Tasbeeh === "kalmaShareef") {
+        kalmaShareef--
+        count.innerHTML = kalmaShareef;
+      }
+      if (acti_Tasbeeh === "duroodEPak") {
+        duroodEPak--
+        count.innerHTML = kalmaShareef;
+      }
+    } 
   }
